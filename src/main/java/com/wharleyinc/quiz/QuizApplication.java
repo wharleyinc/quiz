606ca08fd6c26a05f69761339608f6abe4f5b4b7
MACBOOKPRO\Wharley Inc <walex337@gmail.com>
2022-01-11 18:31:50 +0100

    feat: heroku profile added

--- src/main/java/com/wharleyinc/quiz/QuizApplication.java
+++ src/main/java/com/wharleyinc/quiz/QuizApplication.java
@@ -1,9 +1,12 @@
 package com.wharleyinc.quiz;
 
 import com.wharleyinc.quiz.config.ApplicationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -11,9 +14,22 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public class QuizApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(QuizApplication.class);
+
+    private final Environment env;
+
+    public QuizApplication(Environment env) {
+        this.env = env;
+    }
+
     public static void main(String[] args) {
-        HibernateUtil.generateSchema();
-        SpringApplication.run(QuizApplication.class, args);
+//        HibernateUtil.generateSchema();
+//        SpringApplication.run(QuizApplication.class, args);
+        SpringApplication app = new SpringApplication(QuizApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.debug("This server is running on with profile: {}, \n  with this port; {}", env.getActiveProfiles(), env.getProperty("server.port"));
+
+
     }
 
 }
